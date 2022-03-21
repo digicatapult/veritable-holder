822e444ac2a6a8ba0f97f546e950516378b7675a
dc-andysign <andrei.baloiu@gmail.com>
2022-03-21 08:59:02 +0000

    Added Auth0 domain and clientId data in the global env js file.

--- src/utils/env.js
+++ src/utils/env.js
@@ -3,3 +3,8 @@ export const AUTHORITY_ORIGIN =
   process.env.REACT_APP_AUTHORITY_ORIGIN || 'http://localhost:8051'
 export const HOLDER_ORIGIN =
   process.env.REACT_APP_HOLDER_ORIGIN || 'http://localhost:8031'
+
+export const AUTH0_DOMAIN =
+  process.env.REACT_APP_AUTH0_DOMAIN || 'veritable.eu.auth0.com'
+export const AUTH0_CLIENT_ID =
+  process.env.REACT_APP_AUTH0_CLIENT_ID || 'HIizH1ENhDCIpWp3g0yyIf5rBMCRlFm5'
