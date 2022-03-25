eec51dcb843aef4adb66cca35a8704d38ac57db3
Paulius <paulius.michelevicius@gmail.com>
2022-03-25 15:44:19 +0000

    F2P-47: another config improvement along with script update.

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -3,5 +3,8 @@ const injectDevServer = require('@cypress/react/plugins/react-scripts')
 module.exports = (on, config) => {
   injectDevServer(on, config)
 
+  config.env.customVariable = process.env.CUSTOM_VAR || 'Example'
+  console.log(config)
+
   return config
 }
